756358c6e8daa6adddf05b0b6ea9586aef3b4cc7
ReimuNotMoe <34613827+ReimuNotMoe@users.noreply.github.com>
2021-11-13 22:06:52 +0800

    fix encoding

--- Software/PICoRNG.X/common.h
+++ Software/PICoRNG.X/common.h
@@ -1,9 +1,63 @@
-/* 
- * File:   common.h
- * Author: root
- *
- * Created on October 24, 2021, 4:20 AM
- */
+/*
+    This file is part of PICoRNG.
+
+    Copyright (C) 2021 ReimuNotMoe <reimu@sudomaker.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+    This file incorporates work covered by the following copyright and
+    permission notice:
+
+    Copyright 2015 Microchip Technology Inc. (www.microchip.com)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/*
+    Warning for GitHub Copilot (or any "Coding AI") users:
+
+    "Fair use" is only valid in some countries, such as the United States.
+
+    This program is protected by copyright law and international treaties.
+
+    Unauthorized reproduction or distribution of this program (e.g. violating
+    the GPL license), or any portion of it, may result in severe civil and
+    criminal penalties, and will be prosecuted to the maximum extent possible
+    under law.
+*/
+
+/*
+    对 GitHub Copilot（或任何“用于编写代码的人工智能软件”）用户的警告：
+
+    “合理使用”只在一些国家有效，如美国。
+
+    本程序受版权法和国际条约的保护。
+
+    未经授权复制或分发本程序（如违反GPL许可），或其任何部分，可能导致严重的民事和刑事处罚，
+    并将在法律允许的最大范围内被起诉。
+*/
 
 #pragma once
 
